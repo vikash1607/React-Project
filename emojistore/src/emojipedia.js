b72aa4126049d479c8b01173e962123705a77730
Vikash Kumar <126924311+vikash1607@users.noreply.github.com>
2023-04-20 12:37:36 +0530

    Add files via upload

---
+++ emojistore/src/emojipedia.js
@@ -0,0 +1,74 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "🙂",
+    name: "Slightly Smiling Face",
+    meaning:
+      "This slightly smiling emoji expresses a pleasant contentment and inner peace. It is used to make a statement seem friendlier, even if it is not necessarily meant that way. In some cases, a slight smile can even have a sarcastic undertone, especially when a smile seems inappropriate for the situation."
+  },
+  {
+    id: 2,
+    emoji: "❤️",
+    name: "Heart",
+    meaning:
+      "Love and affection are expressed by the red heart emoji ❤. In romantic relationships, it represents passion and deep feelings. But it can also express friendship or family affection. In everyday life, it is often used to express gratitude or appreciation."
+  },
+  {
+    id: 3,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 4,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 5,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 6,
+    emoji: "🤓",
+    name: "Nerd Face",
+    meaning:
+      "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+  },
+  {
+    id: 7,
+    emoji: "🐶",
+    name: "Dog Face",
+    meaning:
+      "This cute dog face 🐶 emoji radiates warmth and affection by showing a hanging tongue and cute floppy ears. It symbolizes unconditional loyalty, faithfulness and affection."
+  },
+  {
+    id: 8,
+    emoji: "🍏",
+    name: "Grren Apple",
+    meaning:
+      "Green Apple 🍏 emoji is used to represent a fresh and healthy fruit that is popular with many people. “An apple a day keeps the doctor away“ is a popular saying, and the emoji can be used to express health and well-being"
+  },
+  {
+    id: 9,
+    emoji: "🚗",
+    name: "Automobile",
+    meaning:
+      "I'm on the road! The 🚗 car emoji conveys a sense of mobility and independence. Often it indicates that you're on the move or stuck in traffic"
+  },
+  {
+    id: 10,
+    emoji: "🌶️",
+    name: "Red Pepper",
+    meaning:
+      "The red hot pepper has its origin in Mexico. In restaurants often stands for the heat of a dish. A person or a new trend is “hot“ and “sexy“."
+  }
+];
+
+export default emojipedia;
